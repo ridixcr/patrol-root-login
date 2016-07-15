951216f308450f29c59a503bb537ea5479036cfb
ridixcr <pavel@mapbox.com>
2016-07-15 15:29:40 -0500

    ...

--- index.js
+++ index.js
@@ -4,16 +4,40 @@ var message = lcfn.message;
 
 module.exports = {};
 
+var m = module.exports;
+
 module.exports.fn = function (event, callback) {
-    var notification = {
-        subject: 'Root user login in to the console',
-        summary: 'Patrol detected that the root AWS user logged in to the console.',
-        event: event
-    };
-    message(notification, function(err, result) {
-      callback(err, result);
-    });
+    if (m.isRoot(event)) {
+        var notification = {
+            subject: 'Root user login in to the console',
+            summary: 'Patrol detected that the root AWS user logged in to the console.',
+            event: event
+        };
+        message(notification, function (err, result) {
+            callback(err, result);
+        });
+    } else if (m.isOtherUser(event)) {
+        var notification = {
+            subject: 'Other user login in to the console',
+            summary: 'Patrol detected that the other AWS user logged in to the console.',
+            event: event
+        };
+        message(notification, function (err, result) {
+            callback(err, result);
+        });        
+    }else{
+        callback();
+    }
 };
+module.exports.isRoot = function (event) {
+    var userName = event.detail.userIdentity.userName;
+    return userName === 'root';
+};
+module.exports.isOtherUser = function (event) {
+    var userName = event.detail.userIdentity.userName;
+    return userName === 'other';
+};
+
 module.exports.config = {
     name: 'rootLogin',
     eventRule: {
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,20 @@
 var tape = require('tape');
 var rule = require('../index.js');
 
-tape('Logeo de root correctamente detectado', function (t) {
+tape('Logeo de root detectado', function (t) {
     var rootLoginEvent = require('./fixtures/rootLoginEvent.json');
     rule.fn(rootLoginEvent,function (err,message){
-        t.equal(message.subject,'Root user login in to the console','Detected root user login');
+        t.ok(message.subject,'Detected root user login');
+        //alert(rootLoginEvent.detail.userIdentity.userName);
+        //console.log(rootLoginEvent.detail.userIdentity.userName);
+        //t.equal(message.subject,'Root user login in to the console','Detected root user login');        
+        t.end();
+    });
+});
+tape('Logeo de otro usuario detectado', function (t) {
+    var rootLoginEvent = require('./fixtures/otherUserLoginEvent.json');
+    rule.fn(rootLoginEvent,function (err,message){
+        t.ok(message.subject,'Detected other user login');    
         t.end();
     });
 });
